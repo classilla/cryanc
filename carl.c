1f343027d11fc21049f111762cabfb7f3a459978
gattilorenz <gattilorenz@users.noreply.github.com>
2023-03-30 22:40:39 +0200

    Converted MORE C++ comments
    Including the one _I_ introduced :)

--- carl.c
+++ carl.c
@@ -8,7 +8,7 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#if !defined (M_XENIX) || defined(_SCO_DS) //On SCO Unix the next line crashes gcc, but on OpenServer it might work/be needed?
+#if !defined (M_XENIX) || defined(_SCO_DS) /* On SCO Unix the next line crashes gcc, but on OpenServer it might work/be needed? */
 #if !defined(__AUX__) && !defined(__AMIGA__) && (!defined(NS_TARGET_MAJOR) || (NS_TARGET_MAJOR > 3)) && !defined(__MACHTEN_68K__) && !defined(__sun) && !defined(__BEOS__) && (!defined(__hppa) && !defined(__hpux)) && !defined(macintosh)
 #include <sys/select.h>
 #endif
